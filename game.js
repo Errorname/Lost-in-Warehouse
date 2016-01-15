20f8e524d91c0206eb151bbb4cc93b0d34e5fd67
ErrOrnAmE <do.not.press.enter@gmail.com>
2016-01-15 21:12:38 +0100

    Implementing a smooth deplacement

--- game.js
+++ game.js
@@ -34,7 +34,7 @@ BasicGame.Boot.prototype =
 	render: function() {
 		game.debug.text(game.time.fps || '--', 2, 14, "#a7aebe");
 
-		game.debug.text('', 2, 28, "#a7aebe"); //player.coord.x + ":" + player.coord.y
+		game.debug.text(player.coord.x+':'+player.coord.y, 2, 28, "#a7aebe"); //player.coord.x + ":" + player.coord.y
 	},
 	spawnTiles: function() {
 		var tile;
@@ -48,11 +48,101 @@ BasicGame.Boot.prototype =
 	},
 	spawnPlayer: function() {
 		player = game.add.isoSprite(134,134, 0, 'robot', 0, isoGroup);
-		player.anchor.set(0.5);
+		player.anchor.set(0.5, 0.45);
 
 		player.coord = {x: 2, y: 2};
 
-		this.cursors = game.input.keyboard.createCursorKeys();
+		player.keys = [0,0,0,0,0];
+
+		player.move = function() {
+			if (player.canMove()) {
+				player.isMoving = true;
+
+				var tween = game.add.tween(player)
+							.to({isoX: player.coord.x * 67, isoY: player.coord.y * 67}, 
+									//200, Phaser.Easing.Quadratic.InOut, true, 0, 0, false);
+									200, Phaser.Easing.Linear.None, true, 0, 0, false);
+				tween.onComplete.add(function() {
+					player.move();
+				},this)
+			} else {
+				player.isMoving = false;
+			}
+		}
+
+		player.canMove = function() {
+
+			var k = player.lastDirection();
+
+			if (k == -1)
+				return false;
+
+			var x = k == 1 ? -1 : ( k == 3 ? 1 : 0 );
+			var y = k == 2 ? -1 : ( k == 0 ? 1 : 0 );
+
+			player.coord.x += x;
+			player.coord.y += y;
+
+			var isOk = true;
+			if (player.coord.x < 0) {
+				player.coord.x = 0;
+				isOk = false;
+			} else if (player.coord.x > 7) {
+				player.coord.x = 7;
+				isOk = false;
+			}
+			if (player.coord.y < 0) {
+				player.coord.y = 0;
+				isOk = false;
+			} else if (player.coord.y > 7) {
+				player.coord.y = 7
+				isOk = false;
+			}
+			return isOk;
+		}
+
+		player.lastDirection = function() {
+			for (var i = 0; i < 4; i++) {
+				if (player.keys[i] == 2) {
+					return i;
+				}
+			}
+			for (var i = 0; i < 4; i++) {
+				if (player.keys[i] == 1) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		player.registerDirection = function(keyCode) {
+			for (var i = 0; i < 4; i++) {
+				if (player.keys[i] == 2) {
+					player.keys[i] = 1;
+				}
+			}
+			player.keys[keyCode] = 2;
+		}
+
+		game.input.keyboard.onDownCallback = function(event) {
+			// 37 left, 38 up, 39 right, 40 down
+			if (event.keyCode >= 37 && event.keyCode <= 40) {
+
+				player.registerDirection(event.keyCode-37);
+
+				if (!player.isMoving) {
+
+					player.move();
+				}
+			}
+		};
+
+		game.input.keyboard.onUpCallback = function(event) {
+			// 37 left, 38 up, 39 right, 40 down
+			if (event.keyCode >= 37 && event.keyCode <= 40) {
+				player.	keys[event.keyCode-37] = 0;
+			}
+		};
 
 		this.game.input.keyboard.addKeyCapture([
 			Phaser.Keyboard.LEFT,
@@ -60,36 +150,6 @@ BasicGame.Boot.prototype =
 			Phaser.Keyboard.UP,
 			Phaser.Keyboard.DOWN
 		]);
-
-		var up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-		var down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-		var left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-		var right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-
-		up.onDown.add(function() {
-			player.coord.x = player.coord.x > 0 ? player.coord.x-1 : 0;
-
-            player.isoX = player.coord.x * 67;
-		},this);
-
-		down.onDown.add(function() {
-			player.coord.x = player.coord.x < 7 ? player.coord.x+1 : 7;
-
-            player.isoX = player.coord.x * 67;
-		},this);
-
-		left.onDown.add(function() {
-			player.coord.y = player.coord.y < 7 ? player.coord.y+1 : 7;
-
-            player.isoY = player.coord.y * 67;
-		},this);
-
-		right.onDown.add(function() {
-			player.coord.y = player.coord.y > 0 ? player.coord.y-1 : 0;
-
-            player.isoY = player.coord.y * 67;
-		},this);
-
 	}
 };
 
